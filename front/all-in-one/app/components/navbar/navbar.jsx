46e70c010481539092820f6ab76b92c605dc5411
BrunelDev <ahokpossibrunel@gmail.com>
2024-04-03 19:04:21 +0200

    added localisation page

--- front/all-in-one/app/components/navbar/navbar.jsx
+++ front/all-in-one/app/components/navbar/navbar.jsx
@@ -1,6 +1,7 @@
 import "./navbar.css";
 import { Bubbles } from "../logo/logo";
 import { Menu } from "../menu/menu";
+import Link from "next/link";
 export function Navbar({ actual }) {
   return (
     <div className="fixed z-50 left-0 top-0 w-full">
@@ -12,9 +13,13 @@ export function Navbar({ actual }) {
             <li id={actual == "comment ca marche" ? "actual" : ""}>
               Comment ca marche
             </li>
-            <li id={actual == "services" ? "actual" : ""}>Services</li>
+            <Link href="/service">
+              <li id={actual == "services" ? "actual" : ""}>Services</li>
+            </Link>
             <li id={actual == "localisation" ? "actual" : ""}>Localisation</li>
-            <li id={actual == "contact" ? "actual" : ""}>Contact</li>
+            <Link href="/contact">
+              <li id={actual == "contact" ? "actual" : ""}>Contact</li>
+            </Link>
           </ul>
           <div className="menu z-10">
             <Menu />
---
+++ front/all-in-one/app/localisation/page.jsx
@@ -0,0 +1,12 @@
+export default function Localisation() {
+  return (
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d253778.68465830505!2d2.42779949999999!3d6.356242500000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sbj!4v1712163084328!5m2!1sfr!2sbj"
+      width="600"
+      height="450"
+      allowfullscreen=""
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
+  );
+}
